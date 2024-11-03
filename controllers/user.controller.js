766e042c5f3944f21691a23acf256dee044c2c91
mohitkumarr60 <mohitkumarr60@gmail.com>
2024-11-03 12:35:52 +0530

    update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -82,7 +82,7 @@ export const loginUser = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: "Lax",
-      domain: "localhost",
+      domain: "http://localhost:5173",
     });
 
     // send success response
