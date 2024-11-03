ff9f5a2be7c95aa0504fd9352a0e8798f6b549bf
mohitkumarr60 <mohitkumarr60@gmail.com>
2024-11-03 15:12:43 +0530

    update

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 // cross origin resource sharing
 app.use(
   cors({
-    origin: ["https://zestify-qx04ngreq-mohitkumarr60s-projects.vercel.app"],
+    origin: ["https://zestify-rho.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
