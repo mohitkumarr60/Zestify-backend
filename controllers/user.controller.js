a938b94cdcdf57f3d5129d81460ef8e88f0924ac
mohitkumarr60 <mohitkumarr60@gmail.com>
2024-11-03 13:06:59 +0530

    update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -80,7 +80,7 @@ export const loginUser = async (req, res) => {
       expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
       httpOnly: true,
       secure: false,
-      sameSite: "Lax",
+      sameSite: "None",
     });
 
     // send success response
